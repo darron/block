d3aeeaac5aabbe37d24426bc847e80075bfeb64f
Darron Froese <darron@froese.org>
2013-02-21 08:53:59 -0700

    Actually connect to Redis using the string from the command line flags.

--- lib/block.rb
+++ lib/block.rb
@@ -5,7 +5,9 @@ require 'redis'
 # Add requires for other files you add to your project here, so
 # you just need to require this one file in your bin file
 
-def check_for_redis
+def check_for_redis(args)
+  uri = URI.parse(args[:redis])
+  $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
   begin
     $redis.ping
     true
